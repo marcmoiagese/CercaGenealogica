2392d6339912e7537aab3824fa93986f87f588d4
Marc Moya Gese <hevercking@hotmail.com>
2025-12-01 18:22:36 +0100

    millorant pipeline gitlab

--- static/js/menu.js
+++ static/js/menu.js
@@ -94,32 +94,4 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 
-    // MENU IDIOMA
-    /*if (botoSelector && dropdown) {
-        // Obre/tanca el desplegable en fer clic al botó
-        botoSelector.addEventListener('click', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-        });
-
-        // Tanca el desplegable si es fa clic fora
-        document.addEventListener('click', function (event) {
-            if (!botoSelector.contains(event.target) && !dropdown.contains(event.target)) {
-                dropdown.style.display = 'none';
-            }
-        });
-
-        // Opcional: tanca el desplegable quan es selecciona un idioma
-        dropdown.querySelectorAll('a').forEach(link => {
-            link.addEventListener('click', function () {
-                dropdown.style.display = 'none';
-                // Aquí pots afegir codi per canviar l'idioma visualment si vols
-                botoSelector.innerHTML = this.textContent + ' <i class="fas fa-chevron-down"></i>';
-            });
-        });
-    } else {
-        console.warn("No s'han trobat els elements del desplegable d'idioma.");
-    }*/
-
 });
